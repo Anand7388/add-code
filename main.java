98ed4a79cc1cf6f449a90223ec825281284f9b8e
anandyadav38 <yadavanandyadav38@gmail.com>
2024-06-27 16:00:18 +0530

    String.java

--- main.java
+++ main.java
@@ -1,15 +1,13 @@
-import java.util.Scanner;
 public class main {
-    public static void main(String args[]) {
-        int x, y, sum;
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Type a number");
-        x = myObj.nextInt();
-        System.out.println("Type another number");
-        y = myObj.nextInt();
-
-        sum = x + y;
-        System.out.println("sum is:" + sum);
-
+  public static void main(String[] args) {
+    String originalStr = "Anand yadav";
+    String reversedStr = "";
+    System.out.println("Original string: " + originalStr);
+        
+    for (int i = 0; i < originalStr.length(); i++) {
+      reversedStr = originalStr.charAt(i) + reversedStr;
     }
+    
+    System.out.println("Reversed string: "+ reversedStr);
+  }
 }
